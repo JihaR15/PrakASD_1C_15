d8d093641a09876610011fe8fdbcf69ec7069188
“JihaR15 <jiharamdhan97@gmail.com>
2024-02-29 08:19:02 +0700

    P2_percobaan1_1C_15

---
+++ P2/penjualanBuku/Buku15.java
@@ -0,0 +1,27 @@
+package P2.penjualanBuku;
+
+public class Buku15 {
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi() {
+        System.out.println("Judul : " + judul);
+        System.out.println("Pengarang : " + pengarang);
+        System.out.println("Halaman : " + halaman);
+        System.out.println("Stok : " + stok);
+        System.out.println("Harga : " + harga);
+    }
+    
+    void terjual(int jml) {
+        stok -= jml;
+    }
+    
+    void restock(int jml) {
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg) {
+        harga = hrg;
+    }
+}
+
