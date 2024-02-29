ccf3c3f296ce97dcfcac407fcd697f15d2fec9ca
“JihaR15 <jiharamdhan97@gmail.com>
2024-02-29 09:25:17 +0700

    P2_percobaan2_Pertanyaan4_1C_15

--- P2/penjualanBuku/Buku15.java
+++ P2/penjualanBuku/Buku15.java
@@ -1,19 +1,26 @@
 package P2.penjualanBuku;
 
 public class Buku15 {
-    String judul, pengarang;
+    String judul, pengarang, status = "Stok Tersedia";
     int halaman, stok, harga;
 
     void tampilInformasi() {
         System.out.println("Judul : " + judul);
         System.out.println("Pengarang : " + pengarang);
         System.out.println("Halaman : " + halaman);
-        System.out.println("Stok : " + stok);
+        System.out.println("Stok : " + stok );
+        System.out.println("Status : " + status);
         System.out.println("Harga : " + harga);
     }
     
     void terjual(int jml) {
-        stok -= jml;
+        if (stok > 0) {
+            stok -= jml;
+            if (stok <= 0) {
+                stok = 0;
+                status = "Stok Sudah habis";
+            }
+        }
     }
     
     void restock(int jml) {
