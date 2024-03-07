22ba44d39fb54e94bb9c31cabb24319fa61fd704
“JihaR15 <jiharamdhan97@gmail.com>
2024-03-07 09:02:57 +0700

    P3_percobaan2_Pertanyaan2_1C_15

--- P3/AOBpersegiPanjang/ArrayObjects15.java
+++ P3/AOBpersegiPanjang/ArrayObjects15.java
@@ -20,5 +20,29 @@ public class ArrayObjects15 {
             System.out.println("Persegi Panjang ke-" + i + "\nPanjang: " + ppArray[i].panjang + ", lebar: " + ppArray[i].lebar);
         }
 
+        //ARRAY 2D
+
+        PersegiPanjang15[][] ppArray2D = new PersegiPanjang15[2][2];
+
+        for (int i = 0; i < ppArray2D.length; i++) {
+            for (int j = 0; j < ppArray2D[i].length; j++) {
+                ppArray2D[i][j] = new PersegiPanjang15();
+                System.out.println("Persegi Panjang baris ke-" + i + ", kolom ke-" + j);
+                System.out.print("Masukkan panjang: ");
+                ppArray2D[i][j].panjang = sc15.nextInt();
+                System.out.print("Masukkan lebar: ");
+                ppArray2D[i][j].lebar = sc15.nextInt();
+            }
+        }
+        System.out.println("");
+
+        for (int i = 0; i < ppArray2D.length; i++) {
+            for (int j = 0; j < ppArray2D[i].length; j++) {
+                System.out.println("Panjang: " + ppArray2D[i][j].panjang);
+                System.out.println("Lebar: " + ppArray2D[i][j].lebar);
+                System.out.println();
+            }
+        }
+
     }
 }
