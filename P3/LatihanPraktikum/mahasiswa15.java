bfef9d5b0a220b7178a1a52ffea7b19434078e69
“JihaR15 <jiharamdhan97@gmail.com>
2024-03-07 11:48:44 +0700

    P3_LatihanPraktikum_Pertanyaan1_1C_15

---
+++ P3/LatihanPraktikum/mahasiswa15.java
@@ -0,0 +1,36 @@
+package P3.LatihanPraktikum;
+import java.util.Scanner;
+
+public class mahasiswa15 {
+    String nama, nim, jenis_kelamin;
+    double ipk;
+
+    public static void inputDataMhs(mahasiswa15[] mhs) {
+        Scanner sc15 = new Scanner(System.in);
+        for (int i = 0; i < mhs.length; i++) {
+            mhs[i] = new mahasiswa15();
+            System.out.println("== Masukkan data mahasiswa ke-" + (i + 1) + " ==");
+            System.out.print("Masukkan Nama: ");
+            mhs[i].nama = sc15.next();
+            System.out.print("Masukkan NIM: ");
+            mhs[i].nim = sc15.next();
+            System.out.print("Masukkan Jenis Kelamin: ");
+            mhs[i].jenis_kelamin = sc15.next();
+            System.out.print("Masukkan IPK: ");
+            mhs[i].ipk = sc15.nextDouble();
+        }
+        System.out.println("");
+    }
+
+    public static void tampilkanDataMHS(mahasiswa15[] mhs){
+        for (int i = 0; i < mhs.length; i++) {
+            System.out.println("== Data Mahasiswa ke-" + (i+1) );
+            System.out.println("Nama: " + mhs[i].nama);
+            System.out.println("NIM: " + mhs[i].nim);
+            System.out.println("Jenis Kelamin: " + mhs[i].jenis_kelamin);
+            System.out.println("IPK: " + mhs[i].ipk);
+        }
+        System.out.println("");
+    }
+
+}
---
+++ P3/LatihanPraktikum/mahasiswaMain15.java
@@ -0,0 +1,11 @@
+package P3.LatihanPraktikum;
+
+public class mahasiswaMain15 {
+    public static void main(String[] args) {
+        mahasiswa15[] mhs = new mahasiswa15[3];
+        
+        mahasiswa15.inputDataMhs(mhs);
+        mahasiswa15.tampilkanDataMHS(mhs);
+
+    }
+}
