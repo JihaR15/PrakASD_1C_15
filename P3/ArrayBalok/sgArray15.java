e564f9754c2364d909bc98c3d5f5c4d55dae11bd
“JihaR15 <jiharamdhan97@gmail.com>
2024-03-07 09:43:21 +0700

    P3_percobaan3_Pertanyaan4_1C_15

---
+++ P3/ArrayBalok/sgArray15.java
@@ -0,0 +1,13 @@
+package P3.ArrayBalok;
+
+public class sgArray15 {
+    public static void main(String[] args) {
+        Segitiga15[] sgArray = new Segitiga15[4];
+
+        sgArray[0] = new Segitiga15(10, 4);
+        sgArray[1] = new Segitiga15(20, 10);
+        sgArray[2] = new Segitiga15(15, 6);
+        sgArray[3] = new Segitiga15(25, 10);
+
+    }
+}
