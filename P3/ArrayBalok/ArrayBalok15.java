5ebac27cf3dab786f619dfc4dc5ea3b011e2eafd
“JihaR15 <jiharamdhan97@gmail.com>
2024-03-07 09:19:41 +0700

    P3_percobaan3_1C_15

---
+++ P3/ArrayBalok/ArrayBalok15.java
@@ -0,0 +1,15 @@
+package P3.ArrayBalok;
+
+public class ArrayBalok15 {
+    public static void main(String[] args) {
+        Balok15[] blArray = new Balok15[3];
+
+        blArray[0] = new Balok15(100, 30, 12);
+        blArray[1] = new Balok15(120, 40, 15);
+        blArray[2] = new Balok15(210, 50, 25);
+
+        for (int i = 0; i < blArray.length; i++) {
+            System.out.println("Volume balok ke-" + i + ": " + blArray[i].hitungVolume());
+        }
+    }
+}
