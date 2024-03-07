58371cdbdcc2a6a5f0bf25b5b5ae84565db13574
“JihaR15 <jiharamdhan97@gmail.com>
2024-03-07 09:38:48 +0700

    P3_percobaan3_Pertanyaan2_1C_15

---
+++ P3/ArrayBalok/Segitiga15.java
@@ -0,0 +1,11 @@
+package P3.ArrayBalok;
+
+public class Segitiga15 {
+    public int alas;
+    public int tinggi;
+
+    public Segitiga15(int a, int t) {
+        alas = a;
+        tinggi = t;
+    }
+}
