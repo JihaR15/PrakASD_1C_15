380751c8be67ee2f32900ff3c63e7d2f505b1380
“JihaR15 <jiharamdhan97@gmail.com>
2024-04-18 08:43:26 +0700

    P8_percobaan1_pertanyaan4_1C_15

--- P8/Utama15.java
+++ P8/Utama15.java
@@ -5,13 +5,20 @@ import java.util.Scanner;
 public class Utama15 {
     public static void main(String[] args) {
         Scanner scanner15 = new Scanner(System.in);
-        Gudang15 gudang = new Gudang15(7);
+        
+        System.out.println("=== Program Gudang Barang ===");
+        System.out.print("Masukkan Jumlah Barang: ");
+        int jmlBarang = scanner15.nextInt();
+        
+        Gudang15 gudang = new Gudang15(jmlBarang);
+
         while (true) {
-            System.out.println("\nMenu:");
+            System.out.println("Menu:");
             System.out.println("1. Tambah Barang");
             System.out.println("2. Ambil Barang");
             System.out.println("3. Tampilkan tumpukan Barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Lihat Barang Teratas");
+            System.out.println("5. Keluar");
             System.out.print("Pilih Operasi: ");
             int pilihan = scanner15.nextInt();
             scanner15.nextLine();
@@ -35,6 +42,9 @@ public class Utama15 {
                     gudang.tampilkanBarang();
                     break;
                 case 4:
+                    gudang.lihatBarangTeratas();
+                    break;
+                case 5:
                     System.exit(0); // di jobsheet hanya break saja, jadi otomatis looping lagi, tidak keluar
                 default:
                     System.out.println("Pilihan tidak valid. Silakan coba lagi");
