b74d07dc1f9ca965631d257a3f0bf6cedc0c74bb
“JihaR15 <jiharamdhan97@gmail.com>
2024-04-18 08:17:49 +0700

    P8_percobaan1_1C_15

---
+++ P8/Barang15.java
@@ -0,0 +1,13 @@
+package P8;
+
+public class Barang15 {
+    int kode;
+    String nama;
+    String kategori;
+
+    Barang15(int kode, String nama, String kategori) {
+        this.kode = kode;
+        this.nama = nama;
+        this.kategori = kategori;
+    }
+}
---
+++ P8/Utama15.java
@@ -0,0 +1,44 @@
+package P8;
+
+import java.util.Scanner;
+
+public class Utama15 {
+    public static void main(String[] args) {
+        Scanner scanner15 = new Scanner(System.in);
+        Gudang15 gudang = new Gudang15(7);
+        while (true) {
+            System.out.println("\nMenu:");
+            System.out.println("1. Tambah Barang");
+            System.out.println("2. Ambil Barang");
+            System.out.println("3. Tampilkan tumpukan Barang");
+            System.out.println("4. Keluar");
+            System.out.print("Pilih Operasi: ");
+            int pilihan = scanner15.nextInt();
+            scanner15.nextLine();
+
+            switch (pilihan) {
+                case 1:
+                    System.out.print("Masukkan kode Barang: ");
+                    int kode = scanner15.nextInt();
+                    scanner15.nextLine();
+                    System.out.print("Masukkan Nama Barang: ");
+                    String nama = scanner15.nextLine();
+                    System.out.print("Masukkan Nama Kategori: ");
+                    String kategori = scanner15.nextLine();
+                    Barang15 barangBaru = new Barang15(kode, nama, kategori);
+                    gudang.tambahBarang(barangBaru);
+                    break;
+                case 2:
+                    gudang.ambilBarang();
+                    break;
+                case 3:
+                    gudang.tampilkanBarang();
+                    break;
+                case 4:
+                    System.exit(0);
+                default:
+                    System.out.println("Pilihan tidak valid. Silakan coba lagi");
+            }
+        }
+    }
+}
