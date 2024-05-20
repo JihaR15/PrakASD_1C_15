136a3d7796f4af604ee04fe09662ea388514f2ed
“JihaR15 <jiharamdhan97@gmail.com>
2024-05-20 18:19:18 +0700

    P12_percobaan2_1C_15

--- P12/doublelinkedlists/DoubleLinkedListsMain15.java
+++ P12/doublelinkedlists/DoubleLinkedListsMain15.java
@@ -19,5 +19,27 @@ public class DoubleLinkedListsMain15 {
         dll15.clear();
         dll15.print();
         System.out.println("Size : " + dll15.size());
+
+        // Percobaan 2
+
+        System.out.println("==========================");
+        dll15.addLast(50);
+        dll15.addLast(40);
+        dll15.addLast(10);
+        dll15.addLast(20);
+        dll15.print();
+        System.out.println("Size : " + dll15.size());
+        System.out.println("==========================");
+        dll15.removeFirst();
+        dll15.print();
+        System.out.println("Size : " + dll15.size());
+        System.out.println("==========================");
+        dll15.removeLast();
+        dll15.print();
+        System.out.println("Size : " + dll15.size());
+        System.out.println("==========================");
+        dll15.remove(1);
+        dll15.print();
+        System.out.println("Size : " + dll15.size());
     }
 }
