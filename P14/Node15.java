2e5b8d1ade6db867f45379b5acf42aba9fe98bcc
“JihaR15 <jiharamdhan97@gmail.com>
2024-05-30 10:52:15 +0700

    P14_percobaan1_1C_15

---
+++ P14/Node15.java
@@ -0,0 +1,16 @@
+package P14;
+
+public class Node15 {
+    int data;
+    Node15 left, right;
+
+    public Node15(){
+
+    }
+
+    public Node15(int data){
+        this.left = null;
+        this.data = data;
+        this.right = null;
+    }
+}
