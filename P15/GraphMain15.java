ce3c299c3683d9518fbe320e98d490dfecb7a768
“JihaR15 <jiharamdhan97@gmail.com>
2024-06-09 14:35:33 +0700

    P15_percobaan2_1C_15

--- P15/GraphMain15.java
+++ P15/GraphMain15.java
@@ -20,25 +20,38 @@ public class GraphMain15 {
         gedung15.removeEdge(1,3);
         gedung15.printGraph();
         
-        System.out.println("Masukkan gedung asal dan tujuan (ketik 999 pada keduanya untuk keluar)");
-        while (true) {
-            System.out.print("Masukkan gedung asal: ");
-            int asal = sc15.nextInt();
-            System.out.print("Masukkan gedung tujuan: ");
-            int tujuan = sc15.nextInt();
+        // System.out.println("Masukkan gedung asal dan tujuan (ketik 999 pada keduanya untuk keluar)");
+        // while (true) {
+        //     System.out.print("Masukkan gedung asal: ");
+        //     int asal = sc15.nextInt();
+        //     System.out.print("Masukkan gedung tujuan: ");
+        //     int tujuan = sc15.nextInt();
 
-            if (asal == 999 && tujuan == 999) {
-                System.out.println("Program berhenti");
-                break;
-            }
+        //     if (asal == 999 && tujuan == 999) {
+        //         System.out.println("Program berhenti");
+        //         break;
+        //     }
 
-            if (gedung15.isAdjacent(asal, tujuan)) {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
-            } else {
-                System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
-            } 
+        //     if (gedung15.isAdjacent(asal, tujuan)) {
+        //         System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " bertetangga");
+        //     } else {
+        //         System.out.println("Gedung " + (char) ('A' + asal) + " dan " + (char) ('A' + tujuan) + " tidak bertetangga");
+        //     } 
 
+        // Percobaan 2
 
-        }
+        GraphMatriks15 gdg15 = new GraphMatriks15(4);
+        gdg15.makeEdge(0, 1, 50);
+        gdg15.makeEdge(1, 0, 60);
+        gdg15.makeEdge(1, 2, 70);
+        gdg15.makeEdge(2, 1, 80);
+        gdg15.makeEdge(2, 3, 40);
+        gdg15.makeEdge(3, 0, 90);
+        gdg15.printGraph();   
+        System.out.println("Hasil Setelah penghapusan edge");
+        gdg15.removeEdge(2, 1);
+        gdg15.printGraph();
+
     }
+    
 }
